b74b436bacd213742e9dfb7bc637531f99167950
Lucas van Dijk <info@lucasvandijk.nl>
2020-03-29 14:50:36 -0400

    Expose `Kmer` object in Python

--- src/pyfrost/__init__.py
+++ src/pyfrost/__init__.py
@@ -5,8 +5,8 @@ Pyfrost - Python interface to Bifrost with a NetworkX compatible API
 """
 
 from pyfrost.io import *
+from pyfrost.seq import *
 from pyfrost.graph import *
-from pyfrost.utils import *
 
 from ._version import get_versions
 __version__ = get_versions()['version']
--- src/pyfrost/seq.py
+++ src/pyfrost/seq.py
@@ -1,8 +1,8 @@
 """
-:mod:`pyfrost.utils` - Utilities for DNA sequences, graphs, etc.
-================================================================
+:mod:`pyfrost.seq` - Utilities for DNA sequences, kmers, graphs, etc.
+=====================================================================
 """
 
-from bifrost_python import reverse_complement
+from bifrost_python import reverse_complement, Kmer
 
-__all__ = ['reverse_complement']
+__all__ = ['reverse_complement', 'Kmer']
