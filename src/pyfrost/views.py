4279772fc16c784d37de54964fba27ee87b6dddd
Lucas van Dijk <info@lucasvandijk.nl>
2020-04-08 17:42:18 -0400

    Add python module with views

---
+++ src/pyfrost/views.py
@@ -0,0 +1,8 @@
+"""
+:mod:`pyfrost.views` - View classes that provides convenient access to data in the graph
+========================================================================================
+
+"""
+from bifrost_python import NodeView, NodeDataView
+
+__all__ = ['NodeView', 'NodeDataView']
