35f1c2357f80890a5bbd33043d865ea15f5fdbf6
Lucas van Dijk <info@lucasvandijk.nl>
2020-08-11 09:37:47 -0400

    Make function name more consistent

--- src/pyfrost/seq.py
+++ src/pyfrost/seq.py
@@ -7,10 +7,10 @@ from typing import List
 
 from bifrost_python import reverse_complement, Kmer, Strand, set_k
 
-__all__ = ['reverse_complement', 'Kmer', 'Strand', 'set_k', 'sequence_for_path']
+__all__ = ['reverse_complement', 'Kmer', 'Strand', 'set_k', 'path_sequence', 'path_nucleotide_length']
 
 
-def sequence_for_path(g: 'bifrost_python.BifrostDiGraph', path: List[Kmer]) -> str:
+def path_sequence(g: 'bifrost_python.BifrostDiGraph', path: List[Kmer]) -> str:
     """Build the DNA sequence that is spelled by the given path."""
 
     if not path:
@@ -31,3 +31,27 @@ def sequence_for_path(g: 'bifrost_python.BifrostDiGraph', path: List[Kmer]) -> s
         prev = n
 
     return "".join(seqs)
+
+
+def path_nucleotide_length(g: 'bifrost_python.BifrostDiGraph', path: List[Kmer]) -> int:
+    """Compute the length of a path in nucleotides."""
+
+    if not path:
+        return 0
+
+    node_iter = iter(path)
+    start = next(node_iter)
+
+    k = g.graph['k']
+
+    length = g.nodes[start]['length'] + k - 1
+    prev = start
+    for n in node_iter:
+        if (prev, n) not in g.edges:
+            raise ValueError(f"Invalid path specified, ({prev}, {n}) is not an edge.")
+
+        length += g.nodes[n]['length']
+        prev = n
+
+    return length
+
--- tests/test_sequence.py
+++ tests/test_sequence.py
@@ -1,12 +1,19 @@
 import pytest  # noqa
 
-from pyfrost import Kmer, sequence_for_path
+from pyfrost import Kmer, path_sequence, path_nucleotide_length
 
 
 def test_build_sequence(mccortex):
     g = mccortex
 
     path = [Kmer('ACTGA'), Kmer('TCGAT'), Kmer('CGATG')]
-    assert sequence_for_path(g, path) == "ACTGATTTCGATGC"
+    assert path_sequence(g, path) == "ACTGATTTCGATGC"
 
-    assert sequence_for_path(g, []) == ""
+    assert path_sequence(g, []) == ""
+
+
+def test_compute_length(mccortex):
+    g = mccortex
+
+    path = [Kmer('ACTGA'), Kmer('TCGAT'), Kmer('CGATG')]
+    assert path_nucleotide_length(g, path) == 14
