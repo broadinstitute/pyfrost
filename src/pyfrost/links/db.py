4d00e59a9b5d13daa7721f98869f6804b3b59aad
Lucas van Dijk <info@lucasvandijk.nl>
2022-07-29 16:38:00 -0400

    Add function to prune a LinkDB

--- src/pyfrost/links/db.py
+++ src/pyfrost/links/db.py
@@ -4,4 +4,15 @@ Classes and functions to store and create links
 
 from pyfrostcpp import LinkDB, MemLinkDB, MemLinkDBWithCov
 
-__all__ = ['LinkDB', 'MemLinkDB', 'MemLinkDBWithCov']
+__all__ = ['LinkDB', 'MemLinkDB', 'MemLinkDBWithCov', 'prune_db']
+
+
+def prune_db(db: LinkDB, threshold: int):
+    """
+    Prune links with a coverage lower than a given threshold.
+
+    Modifies the given database in-place.
+    """
+
+    for kmer, tree in db.items():
+        tree.prune(threshold)
